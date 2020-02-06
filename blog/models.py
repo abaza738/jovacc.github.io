0436a0320571c4dab4208bf7ebc702b7bab4abb8
abaza738 <abaza.maher.96@gmail.com>
2020-02-06 02:27:59 +0200

    Added ATC and Pilot Connection models in addition to a Member model. Separated Pilot and ATC tables.

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,26 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Member(models.Model):
+    cid = models.IntegerField(unique=True, null=False)
+    name = models.CharField(max_length=80)
+    def __str__(self):
+        return self.cid
+
+class ATCConnection(models.Model):
+    callsign = models.CharField(max_length=40)
+    name = models.ForeignKey(Member, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    def __str__(self):
+        return self.callsign
+
+class PilotConnection(models.Model):
+    callsign = models.CharField(max_length=40)
+    name = models.ForeignKey(Member, on_delete=models.CASCADE)
+    altitude = models.IntegerField()
+    origin = models.CharField(max_length=4, default="-")
+    destination = models.CharField(max_length=4, default="-")
+    def __str__(self):
+        return self.callsign
+
