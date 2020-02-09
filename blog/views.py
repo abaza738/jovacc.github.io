91a2ef28876d1f500179501c9d8d290b43ff8d33
abaza738 <abaza.maher.96@gmail.com>
2020-02-09 13:14:07 +0200

    Regular expressions for online activity in Jordan.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post, Event, PilotConnection, ATCConnection, Member
 from django.utils import timezone
-import json, urllib
+import json, urllib, re
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -15,7 +15,7 @@ def post_list(request):
         online_pilots = []
         online_atc = []
         for i in pilots:
-            if( "OJ" in i['plan']['departure'] or "OJ" in i['plan']['arrival']):
+            if( re.match(r"^OJ[A-Z]{2}$", i['plan']['departure']) or re.match(r"^OJ[A-Z]{2}$", i['plan']['arrival'])):
                 p = PilotConnection()
                 p.cid = i['member']['cid']
                 p.name = i['member']['name']
@@ -24,7 +24,7 @@ def post_list(request):
                 p.callsign = i['callsign']
                 online_pilots.append(p)
         for i in atc:
-            if( "OJ" in i['callsign'] or "AMM" in i['callsign']):
+            if( re.match(r"(^OJ[A-Z]{2}|^AMM_)", i['callsign'])):
                 a = ATCConnection()
                 a.cid = i['member']['cid']
                 a.name = i['member']['name']
